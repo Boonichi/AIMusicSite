902a3cd34cf7bcf1884d2a1da050b37a5265c4b2
Boonichi <pvhung1302@gmail.com>
2023-01-05 20:32:09 +0700

    Refresh

--- Dataset/LyricsLoader.py
+++
@@ -1,8 +0,0 @@
-from Dataset.DataLoader import DataLoader
-
-class LyricsLoader(DataLoader):
-    def __init__(self, args, data):
-        super().__init__(args, data)
-
-    def prepare(self):
-        return 0
