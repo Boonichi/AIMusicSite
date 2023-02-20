f95422b90a6adf8353a617c53af5aa8f7a24e3bc
thanhthien412 <thien.dh412@gmail.com>
2023-02-20 18:38:54 +0700

    adding when user upload file instead of searching directly the song name

--- web/myweb/Kara/views.py
+++ web/myweb/Kara/views.py
@@ -1,36 +1,56 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View 
 from django.urls import reverse
-from django.http import JsonResponse
-
+from django.http import JsonResponse,HttpResponse
+from .models import file
+from .forms import Uploadfile
 from .LYRIC_ALIGN import Lyrics_to_alignment
-from .get_lyric_song import take_lyric_song,get_vocal
+from .get_lyric_song import take_lyric_song,get_vocal,get_lyric
 from .result_to_json import convert_to_json_form,converto_time
 from .libs import os,json
-
+import librosa
 # Create your views here.
 
 class seaching_view(View):
     def get(self,request,*args, **kwargs):
-        return render(request,'Kara/searching.html',{})
+        form=Uploadfile()
+        return render(request,'Kara/searching.html',{'form':form})
      
     def post(self,request,*args, **kwargs):
         global song_search
-        song_search=request.POST['song_name']
+        if('song_name' in request.POST.keys()):
+            song_search=request.POST['song_name']
+        else:
+            song_search=''
+            form=Uploadfile(request.POST,request.FILES)
+            if(form.is_valid()):
+                print(form.cleaned_data)
+                file.objects.create(vocal_file = request.FILES['vocal_file'],lyric_file=request.FILES['lyric_file'])
         return redirect(reverse('Kara:waiting'))
 
 
 class waiting_view(View):
     def get(self,request,*args, **kwargs):
-       
         return render(request,'Kara/waiting.html',{})
     def post(self,request,*args, **kwargs):
         info={}
-        lyric,number,path_song,song_name,author_name=take_lyric_song(song_search)
-        word,treils_length,vocal=Lyrics_to_alignment(path_song,lyric,get_vocal_function=get_vocal).run()
+        global path_song,lyric,song_name,author_name,function_lyric
+        if(song_search!=''):
+            lyric,number,path_song,song_name,author_name=take_lyric_song(song_search)
+            function_lyric=None
+        else:
+            path_song   =os.path.join(os.getcwd(),'media','wav',str(file.objects.all().order_by('-id')[0].vocal_file))
+            lyric       =os.path.join(os.getcwd(),'media','txt',str(file.objects.all().order_by('-id')[0].lyric_file))
+            song_name   ="NULL"
+            author_name ="NULL"
+            function_lyric=get_lyric
+            
 
+        word,treils_length,vocal=Lyrics_to_alignment(path_song,lyric,get_lyric_function=function_lyric,get_vocal_function=get_vocal).run()
         output_file=convert_to_json_form(word,lyric,number,converto_time,treils_length,len(vocal),'static/info','result.json')
+        
         #output_file=convert_to_json_form(0,lyric,number,converto_time,0,0)
+        
         info['lyrics']=output_file
         info['author_name']=author_name
         info['song_name']=song_name
