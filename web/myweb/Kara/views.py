7784b866f3384ecc0e5543c92dc30d8a9571c232
thanhthien412 <thien.dh412@gmail.com>
2023-02-21 16:59:12 +0700

    repair the syntax of Lyric_to_alignment and in result_view add condition when user searching or uploading with different returned path

--- web/myweb/Kara/views.py
+++ web/myweb/Kara/views.py
@@ -24,7 +24,6 @@ class seaching_view(View):
             song_search=''
             form=Uploadfile(request.POST,request.FILES)
             if(form.is_valid()):
-                print(form.cleaned_data)
                 file.objects.create(vocal_file = request.FILES['vocal_file'],lyric_file=request.FILES['lyric_file'])
         return redirect(reverse('Kara:waiting'))
 
@@ -34,42 +33,48 @@ class waiting_view(View):
         return render(request,'Kara/waiting.html',{})
     def post(self,request,*args, **kwargs):
         info={}
-        global path_song,lyric,song_name,author_name,function_lyric
+        global path_song,lyric,song_name,author_name,function_lyric,number
         if(song_search!=''):
             lyric,number,path_song,song_name,author_name=take_lyric_song(song_search)
             function_lyric=None
         else:
-            path_song   =os.path.join(os.getcwd(),'media','wav',str(file.objects.all().order_by('-id')[0].vocal_file))
-            lyric       =os.path.join(os.getcwd(),'media','txt',str(file.objects.all().order_by('-id')[0].lyric_file))
-            song_name   ="NULL"
-            author_name ="NULL"
+            path_song     =os.path.join(os.getcwd(),'media',str(file.objects.all().order_by('-id')[0].vocal_file))
+            lyric         =os.path.join(os.getcwd(),'media',str(file.objects.all().order_by('-id')[0].lyric_file))
+            song_name     ="NULL"
+            author_name   ="NULL"
             function_lyric=get_lyric
+            number        =None
             
-
-        word,treils_length,vocal=Lyrics_to_alignment(path_song,lyric,get_lyric_function=function_lyric,get_vocal_function=get_vocal).run()
-        output_file=convert_to_json_form(word,lyric,number,converto_time,treils_length,len(vocal),'static/info','result.json')
+            
+        word,treils_length,vocal,number,ori_lyric=Lyrics_to_alignment(path_song,lyric,number,get_lyric_function=function_lyric,get_vocal_function=get_vocal).run()
+        output_file=convert_to_json_form(word,ori_lyric,number,converto_time,treils_length,len(vocal),'static/info','result.json')
         
-        #output_file=convert_to_json_form(0,lyric,number,converto_time,0,0)
+        # output_file=convert_to_json_form(0,lyric,number,converto_time,0,0)
         
         info['lyrics']=output_file
         info['author_name']=author_name
         info['song_name']=song_name
-        info['audio']=path_song
-        
         with open(os.path.join(os.getcwd(),'static/info/info_file.json'), "w",encoding='utf-8') as outfile:
             json.dump(info, outfile,ensure_ascii=False)
                             
         return redirect(reverse('Kara:result'))
     
-    
 class result_view(View):
     def get(self,request,*args, **kwargs):
-        json_data = open(os.path.join(os.getcwd(),'static/info/info_file.json'),'rb')
-        data1=json.load(json_data)
-        json_data.close()
-        path=data1['audio'].split(os.path.sep)
-        path=os.path.join(path[-2],path[-1])
-        return render(request,'Kara/result.html',{'path':path})
+        # json_data = open(os.path.join(os.getcwd(),'static/info/info_file.json'),'rb')
+        # data1=json.load(json_data)
+        # json_data.close()
+        # path=data1['audio'].split(os.path.sep)
+        # path=os.path.join(path[-2],path[-1])
+        global path
+
+        if(song_search!=''):
+            path   =path_song.split(os.path.sep)
+            path   =os.path.join(path[-2],path[-1])
+            path   =os.path.join('http://127.0.0.1:8000/','media',path)
+        else:
+            path   =os.path.join('http://127.0.0.1:8000/','media',str(file.objects.all().order_by('-id')[0].vocal_file))
+        return render(request,'Kara/result.html',{'file':path})
     
 class API_JSON(View):
     def get(self,request):
